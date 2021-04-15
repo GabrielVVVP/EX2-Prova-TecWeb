0f406dc10c20700c5a0f9d7f55d10d2dddf360d6
GabrielVVVP <gabrielvvvp@al.insper.edu.br>
2021-04-15 17:33:22 -0300

    pagina memo com GET e POST com ultimo memo conceito B

--- memo/memos/views.py
+++ memo/memos/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Memo
 
-
-def index(request):
-    return render(request, 'memos/index.html')
+def index(request):  
+    if request.method == 'POST':        
+        content = request.POST.get('detalhes')
+        memo = Memo(content=content)
+        memo.save()
+        return redirect('index')
+    elif request.method == 'GET': 
+        all_memos = Memo.objects.all()
+        id_init = 0
+        memo_final = 0
+        for memo in all_memos:
+            if memo.id>id_init:
+                id_init = memo.id
+                memo_final = memo.content         
+        return render(request, 'memos/index.html', {'memo': memo_final})  
+    else:
+        return render(request, 'memos/404.html')
